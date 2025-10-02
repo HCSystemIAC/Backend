9e226e84ef87e2812c9e97b801d8662ad5e7c26b
Melissa <melissayengle@gmail.com>
2025-10-02 05:01:15 -0500

    Modulos de Aurora y Proxy en progreso, por solucionar errores de despliegue

--- infra/envs/dev/providers.tf
+++ infra/envs/dev/providers.tf
@@ -1,14 +1,15 @@
 terraform {
   backend "s3" {
-    bucket         = "hc-tfstate-116981769615-us-east-1"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hc-tf-locks"
-    encrypt        = true
-    kms_key_id     = "alias/hc-tfstate"
+    bucket       = "hc-tfstate-116981769615-us-east-1"
+    key          = "envs/dev/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+    profile      = "hc-dev"
   }
 }
 
 provider "aws" {
-  region = var.region
+  region  = var.region
+  profile = "hc-dev"
 }
